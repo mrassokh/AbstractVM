5d8be4a73bc64ed4420580226f26a509c08fc093
mrassokh <30980569+mrassokh@users.noreply.github.com>
2018-06-28 18:27:02 +0300

    Update Operand.hpp

--- includes/Operand.hpp
+++ includes/Operand.hpp
@@ -52,21 +52,21 @@ template <class T, eOperandType operandType>
          virtual IOperand const * operator % ( IOperand const & rhs ) const; // Modulo
          virtual std::string const & toString( void ) const; // String representation of the instance
 
-	     T                                              		getValue() const;
-	     std::vector<void* (*)(std::string const & value)>      getToValueFunction() const;
-		 std::vector<std::string (*)(double const &leftValue,
-		 				double const &rightValue)>				getResultFunction() const;
-	     OperandFactory*                                		getOperandFactory() const;
+	 T                                              		getValue() const;
+	 std::vector<void* (*)(std::string const & value)>      	getToValueFunction() const;
+	std::vector<std::string (*)(double const &leftValue,
+		 		double const &rightValue)>		getResultFunction() const;
+	 OperandFactory*                                		getOperandFactory() const;
          void                                           		setOperandFactory(const OperandFactory* operandFactory);
 
     private:
-		 T*                                             		m_operand;
+		T*   	                                          	m_operand;
          	OperandFactory*                                		m_operandFactory;
          	eOperandType                                   		m_operandType;
          	int                                            		m_precision;
 
 		 //Output value
-		 std::string                                    		m_toStringOutput;
+		 std::string                                    	m_toStringOutput;
 
 		 //Array of pointers to functions which convert string value to accordingly type
          	std::vector<void* (*)(std::string const & value)>      m_toValueFunc;
@@ -74,34 +74,34 @@ template <class T, eOperandType operandType>
 		 //Array of pointers to ARITHMETIC functions which returns result in string representation
 		 std::vector<std::string (*)(
 			 double const &leftValue,
-			 double const &rightValue)>     					m_resultFunc;
+			 double const &rightValue)>     		m_resultFunc;
 
 		 //Create new m_operand from string value using m_toValueFunc
-		 void*                                          		createMOperand(std::string const & value) const;
+		 void*                                          	createMOperand(std::string const & value) const;
 
 		 //Initialize new Operand class
-		 void                                           		init();
-		 void                                           		first_step_init();
+		 void                                           	init();
+		 void                                           	first_step_init();
 
 		 //Functions which convert string value to accordingly type
-         	static void									*toInt8(std::string const & value);
-         	static void									*toInt16(std::string const & value);
-         	static void									*toInt32(std::string const & value);
-         	static void									*toFloat(std::string const & value);
-         	static void 									*toDouble(std::string const & value);
+         	static void						*toInt8(std::string const & value);
+         	static void						*toInt16(std::string const & value);
+         	static void						*toInt32(std::string const & value);
+         	static void						*toFloat(std::string const & value);
+         	static void 						*toDouble(std::string const & value);
 
 		 //ARITHMETIC functions which returns result in string representation
-		 static std::string								resultAdd(double const & leftValue, double const & rightValue);
-		 static std::string								resultSub(double const & leftValue, double const & rightValue);
-		 static std::string								resultMult(double const & leftValue, double const & rightValue);
-		 static std::string								resultDiv(double const & leftValue, double const & rightValue);
-		 static std::string								resultMod(double const & leftValue, double const &rightValue);
+		 static std::string					resultAdd(double const & leftValue, double const & rightValue);
+		 static std::string					resultSub(double const & leftValue, double const & rightValue);
+		 static std::string					resultMult(double const & leftValue, double const & rightValue);
+		 static std::string					resultDiv(double const & leftValue, double const & rightValue);
+		 static std::string					resultMod(double const & leftValue, double const &rightValue);
 
 		 //Exceptions
-		 static ValueOverflowException 							exceptOverflow;
-		 static ValueUnderflowException 						exceptUnderflow;
-	 	 static DivideByZeroException							exceptDivideZero;
-	 	 static	ModByZeroException							exceptModZero;
-	 	 static	ValueUnderflowOrOverflowException					exceptUnderflowOverflow;
+		 static ValueOverflowException 				exceptOverflow;
+		 static ValueUnderflowException 			exceptUnderflow;
+	 	 static DivideByZeroException				exceptDivideZero;
+	 	 static	ModByZeroException				exceptModZero;
+	 	 static	ValueUnderflowOrOverflowException		exceptUnderflowOverflow;
 	};
 #endif // OPERAND_HPP
